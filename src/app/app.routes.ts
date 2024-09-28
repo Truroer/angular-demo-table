96748db376cd1b6fe5df0741452696f77a7f8df7
Maxim Sinelnikov <maxim.sinelnikov@kps.com>
2024-09-28 22:46:18 +0200

    remove lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,30 @@
 import { Routes } from '@angular/router';
 import { Path } from './interfaces/Path';
+import HomeComponent from './home/home.component';
+import TableDragComponent from './table-drag/table-drag.component';
+import TableGroupedComponent from './table-grouped/table-grouped.component';
+import TableComponent from './table/table.component';
 
 export const routes: Routes = [
   {
     path: Path.Home,
     pathMatch: 'full',
-    loadComponent: () => import('./home/home.component'),
+    // loadComponent: () => import('./home/home.component'),
+    component: HomeComponent
   },
   {
     path: Path.Table,
-    loadComponent: () => import('./table/table.component'),
+    // loadComponent: () => import('./table/table.component'),
+    component: TableComponent
   },
   {
     path: Path.TableDrag,
-    loadComponent: () => import('./table-drag/table-drag.component'),
+    // loadComponent: () => import('./table-drag/table-drag.component'),
+    component: TableDragComponent
   },
   {
     path: Path.TableGrouped,
-    loadComponent: () => import('./table-grouped/table-grouped.component'),
+    // loadComponent: () => import('./table-grouped/table-grouped.component'),
+    component: TableGroupedComponent
   }
 ];
